50b00de51f828a6a8119572be86bc85c0197b864
B0210731 <b0210731@LTB0210731-Mac.local>
2019-06-12 11:12:11 +0530

    Implemented logs and report dependecy

---
+++ src/main/Java/org/utils/TestListener.java
@@ -0,0 +1,99 @@
+package org.utils;
+
+
+import io.appium.java_client.android.AndroidDriver;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import tests.Base_Class;
+
+
+public class TestListener extends Base_Class implements ITestListener {
+
+    private Logger Log = LoggerFactory.getLogger(this.getClass());
+    private static String getTestMethodName(ITestResult iTestResult) {
+        return iTestResult.getMethod().getConstructorOrMethod().getName();
+    }
+
+    public void onStart(ITestContext iTestContext) {
+        System.out.println("I am in onStart method " + iTestContext.getName());
+        iTestContext.setAttribute("WebDriver", this.driver);
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+        System.out.println("I am in onFinish method " + iTestContext.getName());
+        //Do tier down operations for extentreports reporting!
+    }
+    public void onTestStart(ITestResult iTestResult) {
+        Log.info("I am in onTestStart method " +  getTestMethodName(iTestResult) + " start");
+        Log.info(getTestMethodName(iTestResult) + " test is starting.");
+
+    }
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        Log.info("I am in onTestSuccess method " +  getTestMethodName(iTestResult) + " succeed");
+
+
+    }
+
+    //Text attachments for Allure
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshotPNG (AndroidDriver driver) {
+        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    //Text attachments for Allure
+    @Attachment(value = "{0}", type = "text/plain")
+    public static String saveTextLog (String message) {
+        return message;
+    }
+
+    //HTML attachments for Allure
+    @Attachment(value = "{0}", type = "text/html")
+    public static String attachHtml(String html) {
+        return html;
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        System.out.println("I am in onTestFailure method " +  getTestMethodName(iTestResult) + " failed");
+
+        //Get driver from Base_Class and assign to local webdriver variable.
+        Object testClass = iTestResult.getInstance();
+        AndroidDriver driver = ((Base_Class) testClass).getDriver();
+
+        //Allure ScreenShotRobot and SaveTestLog
+        if (driver instanceof AndroidDriver) {
+            System.out.println("Screenshot captured for test case:" + getTestMethodName(iTestResult));
+            saveScreenshotPNG(driver);
+        }
+
+        //Save a log on allure.
+        saveTextLog(getTestMethodName(iTestResult) + " failed and screenshot taken!");
+
+        //Take base64Screenshot screenshot for extent reports
+        String base64Screenshot = "data:image/png;base64,"+((TakesScreenshot)driver).
+                getScreenshotAs(OutputType.BASE64);
+
+
+    }
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+        Log.info("I am in onTestSkipped method "+  getTestMethodName(iTestResult) + " skipped");
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+        Log.info("Test failed but it is in defined success ratio " + getTestMethodName(iTestResult));
+    }
+
+}
+
